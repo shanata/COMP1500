53f5a5e3ab74c50ff0008d75deea1f549cb6ca22
shanata <3434651@likelion.org>
2025-04-03 10:04:05 +0900

    필요없는 부분 주석 처리

--- 01_HelloWorld/Program.cs
+++ 01_HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            /*Console.ReadLine();
+            exe 창이 계속 떠 있게 하는 코드
+            */
         }
     }
 }
