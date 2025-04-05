a9498d255a7ace7bb42ba983df6c3e6122a208fc
shanata <3434651@likelion.org>
2025-04-05 11:16:28 +0900

    2단원까지 코드

---
+++ 02/Comment/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Comment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Console.WriteLine("Hello World!");
+
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Can you hear me?!");
+            */
+        }
+    }
+}
---
+++ 02_DisplayConstants/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DisplayConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(30);
+            Console.WriteLine(30.1);
+            Console.WriteLine(30.2f);
+            Console.Write(true);
+            Console.Write('p');
+        
+        }
+    }
+}
