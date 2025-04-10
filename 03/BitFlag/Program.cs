f3b792c418b6677bab50939bd1174d643b5df0c9
shanata <3434651@likelion.org>
2025-04-11 01:28:07 +0900

    🔧 03_BitFlag 및 Bitwise 연산 학습: 비트 마스크, 시프트 곱셈/나눗셈, 문자열 출력 등

---
+++ 03/BitFlag/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BitFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BIT_FLAG_SIZE = 8;
+
+            byte bitFlags = 0;
+
+            byte mask1 = 1 << 2;
+
+            bitFlags |= mask1;
+
+            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            byte mask3 = (1 << 3) | (1 << 5);
+            bitFlags |= mask3;
+
+            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            bitFlags &= (byte)~mask1;
+
+            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            bitFlags &= 0;
+
+            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            char char1 = 'A';
+            int result1 = char1 | ' ';
+            Console.WriteLine("bitFlags: " + Convert.ToString(char1, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            Console.WriteLine("bitFlags: " + Convert.ToString(' ', 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            Console.WriteLine("result1: " + (char)result1);
+            char char2 = 'a';
+            int result2 = char2 & '_';
+            Console.WriteLine("bitFlags: " + Convert.ToString(char2, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            Console.WriteLine("bitFlags: " + Convert.ToString('_', 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            Console.WriteLine("result2: " + (char)result2);
+        }
+    }
+}
