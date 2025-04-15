94bd18db3fbcc1001180a1fc6c48de1f7b84115f
shanata <3434651@likelion.org>
2025-04-16 00:28:38 +0900

    🧮 2차원 배열 덧셈 구현 및 출력 형식 학습 (05_AddTwo2dArrays)

---
+++ 05/AddTwo2dArrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AddTwo2dArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ROW_COUNT = 4;
+            const int COLUMN_COUNT = 5;
+
+            int[,] array1 = new int[ROW_COUNT, COLUMN_COUNT]
+            {
+                { 2, 3, 4, 10, 5 },
+                { 1, 11, -4, 4, 6 },
+                { -11, -4, 6, -3, -3 },
+                { 7, 17, 2, -4, 2 }
+            };
+
+            int[,] array2 = new int[ROW_COUNT, COLUMN_COUNT]
+            {
+                { -2, 7, 10, -10, 1 },
+                { 2, 7, -6, -13, 6 },
+                { 10, 6, -6, 3, 1 },
+                { 7, 8, 7, -9, 1 }
+            };
+
+            int[,] sumArray = new int[ROW_COUNT, COLUMN_COUNT];
+
+            for (int i = 0; i < ROW_COUNT; i++)
+            {
+                for (int j = 0; j < COLUMN_COUNT; j++)
+                {
+                    sumArray[i, j] = array1[i, j] + array2[i, j];
+                }
+            }
+
+            Console.WriteLine("---Let's print this!---");
+
+            for (int i = 0; i < ROW_COUNT; i++)
+            {
+                for (int j = 0; j < COLUMN_COUNT; j++)
+                {
+                    Console.Write("{0,10} ", sumArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
