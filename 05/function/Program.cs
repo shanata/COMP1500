3a35e34e831ef97228fb9dbcbfe4e83efcad6361
shanata <3434651@likelion.org>
2025-04-23 23:19:44 +0900

    🧩 함수 연습: Add/Subtract/Multiply/거듭제곱 함수 구현 (06_Functions)

---
+++ 05/function/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i < args.Length; i++)
+            {
+                Console.WriteLine($"args[{i}] = {args[i]}");
+            }
+        }
+    }
+}
