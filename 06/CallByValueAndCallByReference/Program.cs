6a4cbb26726d3ae9225290e89aa3f76732715861
shanata <3434651@likelion.org>
2025-04-25 02:49:43 +0900

    📚 스코프(scope)와 🔄 Call by Value vs Call by Reference 학습

---
+++ 06/CallByValueAndCallByReference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CallByValueAndCallByReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+
+            IncrementByValue(num1, 2, 5);
+
+            Console.WriteLine($"num1 after IncrementByValue(): {num1}"); //0
+
+            IncrementByReference(ref num1, 2, 5);
+
+            Console.WriteLine($"num1 after IncrementByReference(): {num1}"); //10
+        }
+
+        static void IncrementByValue(int num, int increment, int incrementCount)
+        {
+            for (int i = 0; i < incrementCount; i++)
+            {
+                num += increment;
+            }
+        }
+
+        static void IncrementByReference(ref int num, int increment, int incrementCount)
+        {
+            for (int i = 0; i < incrementCount; i++)
+            {
+                num += increment;
+            }
+        }
+    }
+}
+
+
---
+++ 06/Scope/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, -3, 4, -10, 4, 2, 9, 15 };
+            double average = GetAverage(numbers);
+
+            Console.WriteLine($"Average : {average}");
+
+            {
+                 //int average = 0; // Compile error!
+                string message = "Message in the first child scope!";
+                Console.WriteLine(message);
+            }
+
+            {
+                //int average = 0; // Compile error!
+                string message = "Message in the second child scope!";
+                Console.WriteLine(message);
+            }
+
+
+        }
+        static double GetAverage(int[] inputs)
+        {
+            int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sum += inputs[i];
+            }
+
+            return (double)sum / inputs.Length;
+        }
+    }
+}
