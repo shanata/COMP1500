0cf8355eba283dc22f0ca422da47c59554c9bda3
shanata <3434651@likelion.org>
2025-05-14 19:30:49 +0900

    📘 12: LINQ, Nullable, Struct, 참조형과 값형 비교 학습 완료

---
+++ 12/ValueTypeVsReferenceType/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ValueTypeVsReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Increment(num);
+
+            Console.WriteLine(num);
+
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Increment(nums);
+
+            Console.WriteLine(string.Join(", ", nums));
+
+            Vector vector = new Vector(0, 0);
+
+            Increment(vector);
+
+            Console.WriteLine($"{vector.X} {vector.Y}");
+        }
+
+        public static void Increment(int num)
+        {
+            num++;
+        }
+
+        public static void Increment(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i ++)
+            {
+                nums[i]++;
+            }
+        }
+
+        public static void Increment(Vector vector)
+        {
+            vector.X++;
+            vector.Y++;
+        }
+    }
+}
